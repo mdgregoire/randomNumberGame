e770cce7a6ab5c9b33e26c24a38d7e19231a7220
Christiana Routon <christiana.routon@gmail.com>
2018-02-08 22:17:22 -0600

    sending and object, not a number

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,20 @@
 $(document).ready(onReady);
 
+class Player{
+  constructor(guess, prevGuess){
+    this.guess = guess,
+    this.prevGuess = prevGuess
+  }
+}
+
+let rando;
+let counter = 0;
+let players = [];
+let player1 = new Player ('None','None');
+let player2 = new Player ('None','None');
+let player3 = new Player ('None','None');
+let player4 = new Player ('None','None');
+
 function onReady(){
   $('#playGame').hide();
   $('#setUpGameButton').on('click', function(event){
@@ -17,3 +32,16 @@ function onReady(){
     }); // end .ajax post
   }) // end onClick
 } //end onReady
+
+function playGame(response){
+  // $('#playGame').show();
+  // $.ajax({
+  //   type:'GET',
+  //   url:'/random'
+  // }).done(function(response){
+  //   rando = response;
+  // }).fail(function(resopnse){
+  //   console.log('we all suck!', rando);
+  // });
+  console.log('in playgame function', response);
+}
--- server/router/randomRouter.js
+++ server/router/randomRouter.js
@@ -1,14 +1,23 @@
 const express = require ('express');
 const router = express.Router();
 const randomModule = require('../modules/randomModule');
+let max;
 
 router.post('/', function(request, response){
-  let max = request.body.max
+  max = request.body.max;
   console.log(max);
 
   let ourRandomNumber=randomModule.makeANumber(max);
-  console.log(ourRandomNumber);
+  console.log('in randomRouter -- post', ourRandomNumber);
+  ourRandomNumberObject = {ourRandomNumber: ourRandomNumber};
+  response.send(ourRandomNumberObject);
   response.sendStatus(200);
 });
+
+// router.get('/', function(request, response){
+//   let rando = randomModule.makeANumber(max);
+//   console.log('in randomRouter get', rando);
+//   response.send(rando);
+// })
 
 module.exports = router;
